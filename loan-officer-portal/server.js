b66b7e6d4055624a6b132abd9ad601c8ed6d12b3
teDdyMucho <tapangjoseangelo@gmail.com>
2025-05-01 12:09:36 -0400

    fixfixxxx

--- loan-officer-portal/server.js
+++ loan-officer-portal/server.js
@@ -6,7 +6,8 @@ const server = http.createServer((req, res) => {
   // --- CORS HEADERS ---
   res.setHeader('Access-Control-Allow-Origin', 'https://mcascrubber.netlify.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
 
   // Handle preflight OPTIONS request
   if (req.method === 'OPTIONS') {
